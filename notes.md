b5f9b67e22a38cbe2fd51d48f519d4ee86f20506
Obair Zahoor <warubair2124@gmail.com>
2026-03-04 15:46:30 +0530

    multiple-route-handlers

--- notes.md
+++ notes.md
@@ -1,4 +1,4 @@
-lec 1
+lec 1 -E02
 - create a repository 
 - initizlize the repository
 - nde_modules,package.json , package-lock.json
@@ -11,7 +11,21 @@ lec 1
 - what is the use of "-g" while npm install
 - difference between caret and tilde (^ vs ~)
 
-lec 2
-- create a git repo. and push all odeto git repo
-- 
+
+lec 2 - E03
+- create a git repo. and push all code to git repo
+- routes and route handlers 
+- install postman and make a workspace /collection , test API calls
+- handles => GET, POST , PATCH , DELETE API CALLS and test them on postman 
+- ecplore routing and use of ? ,= , () , * , in the routing 
+- use of regex in the routes  /a/,/.*fly$/
+- reading the query params in the routes
+- reading the dyamic routes 
+
+
+lec 3 - E05
+- mulltiple route handlers 
+- next()
+- next function and errors along res.send()
+- app.use("/route" ,RH1,RH2,[RH3,RH4],RH5);
 
--- src/app.js
+++ src/app.js
@@ -1,44 +1,28 @@
  const express= require("express");
  const app= express();
 
+app.use("/user",(req,res,next)=>{
+   console.log("route handler 1");
+   // res.send("response 1");
+   next();
+},(req,res,next)=>{
+   console.log("route handler 2");
+   // res.send("response 2");
+   next();
+},(req,res,next)=>{
+   console.log("route handler 3");
+   // res.send("response 3"); 
+   next();
+},(req,res,next)=>{
+   console.log("route handler 4");
+   next();
+});
 
-//creating routes and rout handlers
-//this will match all the HTTP mehod API calls to /test
- app.use("/test",(req,res)=>{
-    res.send("hello from the /test ");
- });
- app.use("/hello",(req,res)=>{
-    res.send("hello hello hello ");
- });
-
- app.use("/newtab",(req,res)=>{
-    res.send("hello from the new tab");
- });
-
-//   app.use("/",(req,res)=>{
-//     res.send("hello from dashboard");
-//  });
-
-
- //this will only handle GET call to /user
- app.get("/user",(req,res)=>{
-   res.send({firstName:"ubair",lastName:"war"});
- });
- 
- app.post("/user",(req,res)=>{
-   //saved data to the DB
-   res.send("Data successfully saved to the database!");
-
- });
-
- app.delete("/user",(req,res)=>{
-   res.send("user deleted successfully!");
- });
 
 
  app.listen(7777,()=>{
     console.log("server is successfully listenining on port 7777...");
 
  });
-
-  
+ 
+ 
